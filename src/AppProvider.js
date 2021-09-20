46c71435bec017d1256850615f3f702e3c16d80e
elmahdim <elmahdi.mahmoud@live.com>
2021-09-20 12:47:03 +0300

    Add initial AppProvider state

--- src/AppProvider.js
+++ src/AppProvider.js
@@ -2,7 +2,35 @@ import React from "react";
 import { AppContext } from ".";
 
 class AppProvider extends React.Component {
-  state = {};
+  state = {
+    items: {
+      item1: {
+        id: 1,
+        title: "100pcs Black Nitrile Gloves Kitchen",
+        price: 10.99,
+        qty: 1,
+      },
+      item2: {
+        id: 2,
+        title: "SOUL PLUS FACE Masks",
+        price: 16.13,
+        qty: 1,
+      },
+      item3: {
+        id: 3,
+        title: "Set Sheets Microfiber Super Soft",
+        price: 17.99,
+        qty: 1,
+      },
+    },
+    itemsCount: {
+      item1: 1,
+      item2: 1,
+      item3: 1,
+    },
+    count: 3,
+    grandTotal: 45.11,
+  };
 
   render() {
     return (

34297ddc25385a8a8addbc0711c5be9300c13d8a
elmahdim <elmahdi.mahmoud@live.com>
2021-09-20 12:17:03 +0300

    define AppContext

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+export const AppContext = React.createContext();
+
 ReactDOM.render(<App />, document.getElementById("root"));
